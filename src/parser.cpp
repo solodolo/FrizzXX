6c4db9e668bd27331912e08b880670292148bad9
Drew Mettlach <dmmettlach@gmail.com>
2020-08-12 22:14:19 -0500

    Add parser method and fix string concatination of error messages

--- src/parser.cpp
+++ src/parser.cpp
@@ -6,6 +6,16 @@
  */
 
 #include "parser.h"
+#include "expressions.h"
+
+void Frizz::Parser::parse() {
+	while(!this->tokens.empty()) {
+		this->next_token();
+
+		this->preamble();
+		this->block();
+	}
+}
 
 void Frizz::Parser::set_tokens(std::vector<Token> tokens) {
 	this->tokens = tokens;
@@ -14,12 +24,14 @@ void Frizz::Parser::set_tokens(std::vector<Token> tokens) {
 void Frizz::Parser::next_token() {
 	if (!this->tokens.empty()) {
 		this->cur_tok = *(this->tokens.begin());
+
+		this->tokens.erase(this->tokens.begin());
 	}
 }
 
 void Frizz::Parser::block() {
 	if(this->optional_found(TokType::tok_block)) {
-		// Do stuff
+
 	}
 }
 
@@ -31,6 +43,7 @@ void Frizz::Parser::preamble() {
 
 bool Frizz::Parser::optional_found(TokType id) {
 	if (id == this->cur_tok.id) {
+		this->last_val = this->cur_tok.value;
 		this->next_token();
 		return true;
 	}
@@ -43,7 +56,7 @@ bool Frizz::Parser::required_found(TokType id) {
 		return true;
 	}
 
-	std::string msg = "Expected " << id << ", Got: " << this->cur_tok.id;
+	std::string msg = "Expected " + std::to_string(id) + ", Got: " + std::to_string(this->cur_tok.id);
 	this->add_error(msg);
 	return false;
 
@@ -55,12 +68,16 @@ bool Frizz::Parser::required_found(TokType id, std::string val) {
 		return true;
 	}
 
-	std::string msg = "Expected " << id << " with val " << val << ", Got: "
-			<< this->cur_tok.id << " with val " << this->cur_tok.value;
+	std::string msg = "Expected " + std::to_string(id) + " with val " + val + ", Got: "
+			+ std::to_string(this->cur_tok.id) + " with val " + this->cur_tok.value;
 	this->add_error(msg);
 	return false;
 }
 
 void Frizz::Parser::add_error(std::string message) {
 	this->errors.push_back(message);
+}
+
+bool Frizz::Parser::has_errors() {
+	return !this->errors.empty();
 }
--- src/parser.h
+++ src/parser.h
@@ -15,9 +15,11 @@ class Parser {
 public:
 	void next_token();
 	void set_tokens(std::vector<Token> tokens);
+	void parse();
 
 private:
 	Token cur_tok;
+	std::string last_val;
 	std::vector<Token> tokens;
 	std::vector<std::string> errors;
 
@@ -28,7 +30,7 @@ private:
 	bool required_found(TokType id);
 	bool required_found(TokType id, std::string val);
 	void add_error(std::string message);
+	bool has_errors();
 };
 }
-
 #endif /* SRC_PARSER_H_ */
