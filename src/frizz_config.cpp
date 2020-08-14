fef5f9138a9511227ee9eafc8401d587ef9793a8
Drew Mettlach <dmmettlach@gmail.com>
2020-08-14 17:21:12 -0500

    Add build path to config

--- src/frizz_config.cpp
+++ src/frizz_config.cpp
@@ -19,8 +19,16 @@ void Frizz::FrizzConfig::load_configuration(std::string path_to_config) {
     if(json_config.contains("static_files_root")) {
         this->static_files_root = json_config["static_files_root"];
     }
+
+    if(json_config.contains("build_path")) {
+        this->build_path = json_config["build_path"];
+    }
 }
 
 std::string Frizz::FrizzConfig::get_static_files_root() {
     return this->static_files_root;
+}
+
+std::string Frizz::FrizzConfig::get_build_path() {
+    return this->build_path;
 }
--- src/frizz_config.h
+++ src/frizz_config.h
@@ -15,9 +15,11 @@ namespace Frizz {
         public:
             void load_configuration(std::string path_to_config);
             std::string get_static_files_root();
+            std::string get_build_path();
 
         private:
             std::string static_files_root;
+            std::string build_path;
     };
 }
 
