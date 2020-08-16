3f269474d2f15236da1ab23cd1bca9e222212fe7
Drew Mettlach <dmmettlach@gmail.com>
2020-08-15 23:18:42 -0500

    Move test.md to subfolder

--- tests/structure_tests.cpp
+++ tests/structure_tests.cpp
@@ -10,7 +10,7 @@
 
 #include "structures.h"
 
-static const std::string TEST_FILE_DIR = "./tests/test_files";
+static const std::string TEST_FILE_DIR = "./tests/test_files/sources";
 
 std::string get_expected(std::filesystem::path path) {
   std::ifstream input(path);
@@ -31,6 +31,6 @@ TEST(AssignmentStructTests, ReadsFileContents) {
   p /= "test1.md";
 
   std::string expected = get_expected(p);
-  
+
   ASSERT_EQ(expected, contents);
 }
