a774243902c129fd6cd4283e6f7b3709f0fccce4
Drew Mettlach <dmmettlach@gmail.com>
2020-08-16 21:06:55 -0500

    Add getter for partial config path directory

--- src/frizz_config.cpp
+++ src/frizz_config.cpp
@@ -10,39 +10,47 @@
 #include "frizz_config.h"
 #include "nlohmann/json.hpp"
 
-void Frizz::FrizzConfig::load_configuration(std::string path_to_config) {
+bool Frizz::FrizzConfig::load_configuration(std::string path_to_config) {
     std::ifstream input(path_to_config);
     nlohmann::json json_config;
 
     input >> json_config;
 
     if(json_config.contains(Frizz::FrizzConfig::BASE_TEMPLATES_PATH_KEY)) {
-        this->source_root_path = json_config[Frizz::FrizzConfig::BASE_TEMPLATES_PATH_KEY];
+        std::string p = json_config[Frizz::FrizzConfig::BASE_TEMPLATES_PATH_KEY];
+        this->source_root_path = std::filesystem::path(p);
+    }
+    else {
+        return false;
+    }
+
+    if(json_config.contains(Frizz::FrizzConfig::PARTIAL_TEMPLATES_PATH_KEY)) {
+        std::string p = json_config[Frizz::FrizzConfig::PARTIAL_TEMPLATES_PATH_KEY];
+        this->partial_templates_path = std::filesystem::path(p);
+    }
+    else {
+        return false;
     }
 
     if(json_config.contains(Frizz::FrizzConfig::BUILD_PATH_KEY)) {
-        this->build_path = json_config[Frizz::FrizzConfig::BUILD_PATH_KEY];
+        std::string p = json_config[Frizz::FrizzConfig::BUILD_PATH_KEY];
+        this->build_path = std::filesystem::path(p);
     }
+    else {
+        return false;
+    }
+
+    return true;
 }
 
-std::string Frizz::FrizzConfig::get_source_root_path() {
+std::filesystem::path Frizz::FrizzConfig::get_source_root_path() {
     return this->source_root_path;
 }
 
-std::string Frizz::FrizzConfig::get_build_path() {
+std::filesystem::path Frizz::FrizzConfig::get_partial_templates_path() {
+    return this->partial_templates_path;
+}
+
+std::filesystem::path Frizz::FrizzConfig::get_build_path() {
     return this->build_path;
-}
-
-std::filesystem::path Frizz::FrizzConfig::get_path_to_input_file(std::string filename) {
-    std::filesystem::path path(this->get_source_root_path());
-    path /= filename;
-
-    return path;
-}
-
-std::filesystem::path Frizz::FrizzConfig::get_path_to_output_file(std::string filename) {
-    std::filesystem::path path(this->get_build_path());
-    path /= filename;
-
-    return path;
 }
--- src/frizz_config.h
+++ src/frizz_config.h
@@ -14,19 +14,19 @@
 namespace Frizz {
 class FrizzConfig {
 public:
-  void load_configuration(std::string path_to_config);
-  std::string get_source_root_path();
-  std::string get_build_path();
-  std::filesystem::path get_path_to_input_file(std::string filename);
-  std::filesystem::path get_path_to_output_file(std::string filename);
+  bool load_configuration(std::string path_to_config);
+  std::filesystem::path get_source_root_path();
+  std::filesystem::path get_partial_templates_path();
+  std::filesystem::path get_build_path();
 
 private:
   inline const static std::string BASE_TEMPLATES_PATH_KEY = "base_templates_path";
   inline const static std::string PARTIAL_TEMPLATES_PATH_KEY = "partial_templates_path";
   inline const static std::string BUILD_PATH_KEY = "build_path";
   
-  std::string source_root_path;
-  std::string build_path;
+  std::filesystem::path source_root_path;
+  std::filesystem::path partial_templates_path;
+  std::filesystem::path build_path;
 };
 }
 
