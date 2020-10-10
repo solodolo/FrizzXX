fe1df36cff0907fc5bde3b777091f8622287d4f3
Drew Mettlach <dmmettlach@gmail.com>
2020-10-09 20:30:42 -0500

    Allow visitors to accept const ast references

--- src/syntax_tree_visitors.cpp
+++ src/syntax_tree_visitors.cpp
@@ -11,26 +11,26 @@
   ########### AstVisitor ##########
 */
 
-std::tuple<std::string, std::string> Frizz::AstVisitor::visit(Frizz::BasicAst& ast) {
+std::tuple<std::string, std::string> Frizz::AstVisitor::visit(const Frizz::BasicAst& ast) {
   return std::make_tuple("", "");
 }
 
-std::tuple<std::string, std::string> Frizz::AstVisitor::visit(Frizz::AssignmentAst& ast) {
+std::tuple<std::string, std::string> Frizz::AstVisitor::visit(const Frizz::AssignmentAst& ast) {
   // src="blah.md" should be used to place a partial as-is inline
   // therefore src assignments should only return info when accessed by FileContentVisitor
   if(ast.is_src()) {
     return std::make_tuple("", "");
   }
-  
+
   // not a source so return key and value as-is
   return std::make_tuple(ast.get_name(), ast.get_value());
 }
 
-std::tuple<std::string, std::string> Frizz::AstVisitor::visit(Frizz::ForLoopAst& ast) {
+std::tuple<std::string, std::string> Frizz::AstVisitor::visit(const Frizz::ForLoopAst& ast) {
   return std::make_tuple("", "");
 }
 
-std::tuple<std::string, std::string> Frizz::AstVisitor::visit(Frizz::PassthroughAst& ast) {
+std::tuple<std::string, std::string> Frizz::AstVisitor::visit(const Frizz::PassthroughAst& ast) {
   // Return the value unmodified
   return std::make_tuple("", ast.get_value());
 }
@@ -38,7 +38,7 @@ std::tuple<std::string, std::string> Frizz::AstVisitor::visit(Frizz::Passthrough
 /*
   ########### FileContentVisitor ##########
 */
-std::string Frizz::FileContentVisitor::visit(Frizz::AssignmentAst& ast) {
+std::string Frizz::FileContentVisitor::visit(const Frizz::AssignmentAst& ast) {
   std::string contents;
 
   if(ast.is_src()) {
@@ -53,19 +53,19 @@ std::string Frizz::FileContentVisitor::visit(Frizz::AssignmentAst& ast) {
   ########### ContextVisitor ##########
 */
 std::tuple<std::string, std::filesystem::path> Frizz::ContextVisitor::visit(
-  Frizz::AssignmentAst& ast) {
+  const Frizz::AssignmentAst& ast) {
 
-  std::string parent_name = ast.get_parent_name();
+  std::string name_space = ast.get_namespace();
   std::filesystem::path context_filepath = ast.get_context_filepath();
 
-  return std::make_tuple(parent_name, context_filepath);
+  return std::make_tuple(name_space, context_filepath);
 }
 
 /*
   ########## ContextReplacementVisitor ##########
 */
 std::string Frizz::ContextReplacementVisitor::visit(
-  CtxReplacementAst& ast, std::unordered_map<std::string, std::string> context) {
+  const CtxReplacementAst& ast, std::unordered_map<std::string, std::string> context) {
   std::string lookup_key = ast.get_namespaced_key();
   auto got = context.find(lookup_key);
 
@@ -79,13 +79,13 @@ std::string Frizz::ContextReplacementVisitor::visit(
 /*
   ########## ContextChildrenVisitor ##########
 */
-std::vector<std::shared_ptr<Frizz::BasicAst>> Frizz::ContextChildrenVisitor::visit(
-  Frizz::BasicAst& ast) {
-  return std::vector<std::shared_ptr<Frizz::BasicAst>>();
+std::vector<std::reference_wrapper<const Frizz::BasicAst>> Frizz::ContextChildrenVisitor::visit(
+  const Frizz::BasicAst& ast) {
+  return std::vector<std::reference_wrapper<const Frizz::BasicAst>>();
 }
 
-std::vector<std::shared_ptr<Frizz::BasicAst>> Frizz::ContextChildrenVisitor::visit(
-  Frizz::ForLoopAst& ast) {
+std::vector<std::reference_wrapper<const Frizz::BasicAst>> Frizz::ContextChildrenVisitor::visit(
+  const Frizz::ForLoopAst& ast) {
 
   return ast.get_children();
 }
